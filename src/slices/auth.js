91ce75026d99a2e20627d3c8941189209566e423
Manuel Rodriguez <manuelrodriguez@192.168.1.9>
2022-05-17 23:08:40 -0300

    now persisting token in mobile storage

--- src/slices/auth.js
+++ src/slices/auth.js
@@ -1,9 +1,10 @@
 import {createSlice} from '@reduxjs/toolkit';
 import 'react-native-get-random-values';
 import {v4 as uuidv4} from 'uuid';
+import {storage} from '../storage/storage';
 
 export const initialState = {
-  token: undefined,
+  token: storage.getString('token'),
 };
 
 const authSlice = createSlice({
@@ -24,8 +25,11 @@ export const {login, logout} = authSlice.actions;
 export const tokenSelector = state => state.auth.token;
 
 export function LoginUser() {
+  if (storage.getString('token') === undefined) {
+    storage.set('token', uuidv4());
+  }
   return dispatch => {
-    const token = uuidv4();
+    const token = storage.getString('token');
     dispatch(login(token));
   };
 }
